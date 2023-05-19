2b53294787f1ebf7dc00bd23e6af50b8cb3aa2bc
zhiwei.li <zhiwei.li@xjsd.com>
2023-05-19 17:54:22 +0800

    添加配置文件

--- DurationPlugin/src/main/kotlin/com/upuphone/asm/TimeCostPlugin.kt
+++ DurationPlugin/src/main/kotlin/com/upuphone/asm/TimeCostPlugin.kt
@@ -7,15 +7,28 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class TimeCostPlugin : Plugin<Project> {
+    companion object {
+        var list = listOf<String>()
+    }
+    
     override fun apply(project: Project) {
         val androidComponents = project.extensions.getByType(AndroidComponentsExtension::class.java)
+        //获取可以通过的包名集合
+        val strList: String = project.properties["duration_filter"] as String
+        list = strList.split("/")
+        println("##########$list")
         androidComponents.onVariants { variant ->
             variant.instrumentation.transformClassesWith(
                 TimeCostTransform::class.java,
-                    InstrumentationScope.ALL) {}
+                InstrumentationScope.ALL
+            ) {}
             variant.instrumentation.setAsmFramesComputationMode(
-                    FramesComputationMode.COMPUTE_FRAMES_FOR_INSTRUMENTED_METHODS
+                FramesComputationMode.COMPUTE_FRAMES_FOR_INSTRUMENTED_METHODS
             )
         }
+    }
+    
+    class MyPluginExtensionsExtensions {
+        lateinit var duration_filter: List<String>
     }
 }
--- DurationPlugin/src/main/kotlin/com/upuphone/asm/TimeCostTransform.kt
+++ DurationPlugin/src/main/kotlin/com/upuphone/asm/TimeCostTransform.kt
@@ -4,11 +4,22 @@ import com.android.build.api.instrumentation.*
 import org.objectweb.asm.ClassVisitor
 
 abstract class TimeCostTransform : AsmClassVisitorFactory<InstrumentationParameters.None> {
+    
     override fun createClassVisitor(classContext: ClassContext, nextClassVisitor: ClassVisitor): ClassVisitor {
         return TimeCostClassVisitor(nextClassVisitor, classContext.currentClassData.className)
     }
     
     override fun isInstrumentable(classData: ClassData): Boolean {
-        return classData.className.startsWith("com.upuphone")
+        //如果没有过滤条件，就执行全部类，效率较低
+        if (TimeCostPlugin.list.isNotEmpty()) {
+            return true
+        }
+        //过滤条件
+        TimeCostPlugin.list.forEach {
+            if (classData.className.startsWith(it.trim())) {
+                return true
+            }
+        }
+        return false
     }
 }
